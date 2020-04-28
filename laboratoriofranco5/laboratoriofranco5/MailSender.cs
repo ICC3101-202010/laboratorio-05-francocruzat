e86a25d3f967af39b9f6291b17742635164775f9
Franco Cruzat <fhcruzat@miuandes.cl>
2020-04-28 15:15:35 -0300

    avanzefinal

--- laboratoriofranco5/laboratoriofranco5/MailSender.cs
+++ laboratoriofranco5/laboratoriofranco5/MailSender.cs
@@ -26,6 +26,8 @@ namespace laboratoriofranco5
             Thread.Sleep(2000);
             Console.WriteLine($"\nCorreo enviado a {e.Email}: \n Gracias por registrarte, {e.Username}!\n Por favor, para poder verificar tu correo, has click en: {e.VerificationLink}\n");
             Thread.Sleep(2000);
+            OnEmailSent();
+           
         }
 
         public void OnPasswordChanged(object source, ChangePasswordEventArgs e)
@@ -35,5 +37,5 @@ namespace laboratoriofranco5
             Thread.Sleep(2000);
         }
 
-    }
+    }  
 }
