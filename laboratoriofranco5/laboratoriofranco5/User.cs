d5e2c2562d500bb6c657368df779ea6133aa198b
Franco Cruzat <fhcruzat@miuandes.cl>
2020-04-28 11:16:19 -0300

    avanze

---
+++ laboratoriofranco5/laboratoriofranco5/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace laboratoriofranco5
+{
+    public class User
+    {
+        string Vc;
+        //Creamos el delegate para el evento EmailVerified
+        public delegate void VerifiedEmail(object source,EventArgs args);
+        //declaramos evento
+        public event VerifiedEmail EmailVerified;
+
+        protected virtual void OnEmailVerified()
+        {
+            if (EmailVerified != null)
+            {
+                Console.WriteLine("Desea verificar su correo? Ingrese si o no");
+                Vc = Console.ReadLine();
+                if (Vc == "si")
+                {
+                    //mandamos la señal
+                    EmailVerified(this, EventArgs.Empty);
+                }
+            }
+
+
+        }
+
+
+
+    }
+}
